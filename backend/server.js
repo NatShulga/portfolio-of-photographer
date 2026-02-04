fd18b9fe66238479f44ffffb685562b3bef31995
Natalya Shulga <mrs.flowerfields@gmail.com>
2026-02-04 16:34:23 +0300

    fix пути

--- backend/server.js
+++ backend/server.js
@@ -24,16 +24,16 @@ const pool = new Pool({
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'frontend')));
+app.use(express.static(path.join(__dirname, '..', 'frontend')));
 
 const upload = multer({ storage: multer.memoryStorage() });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend','index.html'));
+    res.sendFile(path.join(__dirname, '..', 'frontend', 'index.html'));
 });
 
 app.get('/admin', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend', 'admin.html'));
+    res.sendFile(path.join(__dirname, '..', 'frontend', 'admin.html'));
 });
 
 
