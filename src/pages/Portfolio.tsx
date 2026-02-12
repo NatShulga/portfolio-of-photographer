13f8283f1a29150e5d09cbfeaeb191fafe5e1acb
online shop <dove.sh@yandex.ru>
2026-02-12 17:31:54 +0300

    тест

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -180,7 +180,7 @@ export const Portfolio = () => {
                               }}
                               className="cursor-pointer text-red-300 hover:text-red-500 transition-colors"
                             >
-                              [ Удалить ]
+                              
                             </button>
                           )}
                         </div>   
